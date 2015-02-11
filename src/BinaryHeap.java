6ae79b7d1b63d044c226f19d16915d112eef3e71
Sam Noyes <eliotman24@comcast.net>
2015-02-10 22:56:07 -0500

    Began the method that adds an object to the heap

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -6,4 +6,26 @@ import java.util.ArrayList;
 public class BinaryHeap<E> {
     ArrayList<E> arr;//Maybe should be a different data type?
 
+    public void addObject(BinaryHeapNode<E> rt, E o) {//UNFINISHED - add object o to the heap rt
+        if (isLeft()) {
+            addObject(rt.getLeft(), o);
+        }
+        else {
+            addObject(rt.getRight(), o);
+        }
+    }
+
+    public boolean isLeft() {
+        return (2^(numRows()+1))-(arr.size()-(2^numRows()))>(2^(numRows()+1))/2;
+    }
+
+    public int numRows() {
+        int num = 0;
+        int tempArrSize = arr.size();
+        while (tempArrSize>(2^num)) {
+            tempArrSize -= 2^num;
+            num++;
+        }
+        return num;
+    }
 }
---
+++ src/BinaryHeapNode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by smurphy on 2/10/15.
+ */
+public class BinaryHeapNode<E> {
+    private BinaryHeapNode<E> left;
+    private BinaryHeapNode<E> right;
+    public BinaryHeapNode<E> getLeft() {
+        return left;
+    }
+    public BinaryHeapNode<E> getRight() {
+        return right;
+    }
+}

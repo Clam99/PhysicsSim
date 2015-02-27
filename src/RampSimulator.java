f760bbe9c211ba1f18390d8e374fedf8179917ed
Sam Noyes <eliotman24@comcast.net>
2015-02-27 14:33:37 -0500

    Commented the RampSimulator class

--- src/RampSimulator.java
+++ src/RampSimulator.java
@@ -8,23 +8,22 @@ public class RampSimulator extends Simulator {
     Ball ball;
     Ramp ramp;
     public Graph graph;
-    double initialBallX;
+    double initialBallX;//Initial position of ball
     double initialBallY;
-    double angle;
-    double rampLen;
+    double angle;//angle of ramp with the floor
+    double rampLen;//length of ramp hypotenuse
     double gF;//gravitational field
     double ballRadius;
     double ballMass;
-   // RampSimLogic logic;
 
     public RampSimulator(double a, double rampLen, double ballMass, double ballRadius, double w, double h, Graph g, double gF, Canvas p) {
-        //super();
+        super();
         graph = g;
         simHeight = h;
         simWidth = w;
         angle = a;
         this.rampLen = rampLen;
-        op = new RampOptionsPanel(this, p);
+        op = new RampOptionsPanel(this, p);//Create the options panel
         this.gF = gF;
         this.ballRadius = ballRadius;
         this.ballMass = ballMass;
@@ -35,8 +34,10 @@ public class RampSimulator extends Simulator {
             g.setColor(ramp.c);
             g.fillPolygon(ramp);
             g.setColor(Color.blue);
-            g.fillOval((int) (ball.getBallLogic().getX() - ball.getBallLogic().getRadius()), (int) (ball.getBallLogic().getY() - ball.getBallLogic().getRadius()), (int) ball.getBallLogic().getRadius() * 2, (int) ball.getBallLogic().getRadius() * 2);
+            g.fillOval((int) (ball.getBallLogic().getX() - ball.getBallLogic().getRadius()), (int) (ball.getBallLogic().getY() - ball.getBallLogic().getRadius()), (int) ball.getBallLogic().getRadius() * 2, (int) ball.getBallLogic().getRadius() * 2);//draw and fill the ball centered at getX() and getY()
             g.setColor(Color.black);
+
+            //fill the right wall and floor to make it obvious where they are
             g.fillRect((int) ((RampSimLogic) logic).floor.getX1(), (int) ((RampSimLogic) logic).floor.getY1(), (int) ((RampSimLogic) logic).floor.getX2() - (int) ((RampSimLogic) logic).floor.getX1(), 200);
             g.fillRect((int) ((RampSimLogic) logic).rightWall.getX1(), (int) ((RampSimLogic) logic).rightWall.getY1(), 200, (int) ((RampSimLogic) logic).rightWall.getY2() - (int) ((RampSimLogic) logic).rightWall.getY1());
         }
@@ -45,29 +46,29 @@ public class RampSimulator extends Simulator {
 
 
     @Override
-    public void updateGUI() {
+    public void updateGUI() {//update the GUI and call logic's update method as well
         repaint();
         ((RampSimLogic)logic).update();
     }
 
 
-    public void makeRamp() {
-        int[] xpoints = {0,0, (int)(Math.cos(angle)*rampLen)};
-        int[] ypoints = { (int)(-Math.sin(angle)*rampLen+simHeight), (int)simHeight, (int)simHeight};//In order from top left, bottom left, bottom right
+    public void makeRamp() {//draw the ramp
+        int[] xpoints = {0,0, (int)(Math.cos(angle)*rampLen)};//Vertices in order from top left, bottom left, bottom right
+        int[] ypoints = { (int)(-Math.sin(angle)*rampLen+simHeight), (int)simHeight, (int)simHeight};
 
         ramp = new Ramp(xpoints, ypoints, 3, Color.red);
         initialBallX = ballRadius*Math.cos(Math.toRadians(90)-angle);
         initialBallY = simHeight-Math.sin(angle)*rampLen-ballRadius*Math.sin(Math.toRadians(90)-angle);
-        ball = new Ball(0,Math.toRadians(360)-angle,ballMass,initialBallX,initialBallY, ballRadius, 1, gF, fps);
+        ball = new Ball(0,Math.toRadians(360)-angle,ballMass,initialBallX,initialBallY, ballRadius, 1, gF, fps);//create the ball
     }
 
-    public void startRecording(String str, String str2, double angle) {
+    public void startRecording(String str, String str2, double angle) {//Start the sim and begin graphing - takes the variables to graph and the angle of the ramp as parameters
         this.angle = Math.toRadians(angle);
         makeRamp();
         logic = new RampSimLogic(ball, ramp, graph, angle, rampLen, simWidth, simHeight, fps);
 
         repaint();
-        super.startRecording(str, str2);
+        super.startRecording(str, str2);//call the parent startRecording method
     }
 
 }
